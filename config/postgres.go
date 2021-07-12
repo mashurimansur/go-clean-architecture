aea5d6654ba479deeef2a1615cb3b7308646555d
Hury <mashurimansur18@gmail.com>
2021-07-12 09:46:31 +0800

    feat(sqlite): :zap: add sqlite db for testing

--- config/postgres.go
+++ config/postgres.go
@@ -3,8 +3,10 @@ package config
 import (
 	"clean-arch-go/exception"
 	"fmt"
+	"time"
 
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -17,6 +19,24 @@ func NewPostgreConnection(config Config) *gorm.DB {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Makassar", host, username, password, database, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	exception.PanicIfNeeded(err)
+
+	sqlDB, _ := db.DB()
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return db
+}
+
+func NewSQLiteConnection() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	exception.PanicIfNeeded(err)
 
 	return db
